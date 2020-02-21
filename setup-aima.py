63a96fef9eb58f963a5339ab525e81bfd84556c8
Ian Park <contact@duckonomy.com>
2020-02-21 12:20:43 -0500

    sortedcontainers

--- setup-aima.py
+++ setup-aima.py
@@ -4,5 +4,5 @@ setup(
     name='aima',
     version='1.0.0',
     packages=find_packages(include=['aima']),
-    install_requires=['networkx==1.11', 'jupyter', 'tqdm', 'numpy'],
+    install_requires=['networkx==1.11', 'jupyter', 'tqdm', 'numpy', 'sortedcontainers'],
 )
