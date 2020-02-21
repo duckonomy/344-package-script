d8d7720191a3ab3bb80b197ec060cc8f4362f216
Ian Park <contact@duckonomy.com>
2020-02-21 11:55:33 -0500

    import fix

--- install.sh
+++ install.sh
@@ -34,8 +34,8 @@ for entry in "$search_dir"$BUILD_DIR/aima/*.py
 do
   previous_import=$(basename "$entry" | cut -d. -f1)
   echo $previous_import
-  find $BUILD_DIR/aima/*.py -type f -exec sed -i -e "s/from\ $previous_import/from\ \.$previous_import/g" {} \;
-  find $BUILD_DIR/aima/*.py -type f -exec sed -i -e "s/import\ $previous_import/import\ \.$previous_import/g" {} \;
+  find $BUILD_DIR/aima/*.py -type f -exec sed -i -e "s/from\ $previous_import/from\ aima\.$previous_import/g" {} \;
+  find $BUILD_DIR/aima/*.py -type f -exec sed -i -e "s/import\ $previous_import/import\ aima\.$previous_import/g" {} \;
   echo "Complete replacing"
 done
 
